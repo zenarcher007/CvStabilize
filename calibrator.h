c61e60f944a3b76d22fe9b7a16330eaa6c8f9164
zenarcher007 <jdouty03@icloud.com>
2024-11-24 20:23:06 +0100

    Add incomplete calibrator

--- calibrator.h
+++ calibrator.h
@@ -14,7 +14,7 @@ private:
   cv::VideoCapture cap;
   PointCloudTracker pct;
 
-  // Reads the passed-in vector, and returns a unique_ptr to a vector of the same size, with each point in the new vector being positionally-independent from the original
+  // Reads the passed-in vector, and returns a unique_ptr to a new vector of the same size, with each point in the new vector being positionally-independent from the original
   std::unique_ptr<std::vector<cv::Point2f>> getPositionalIndependent(const std::vector<cv::Point2f>& points) {
     // Find the average point
     cv::Point2f average;
@@ -48,14 +48,25 @@ public:
     this->cap = cap;
   }
     
+  // Loss function: Advance the frame, 
+  void lossFunction() {
+    
+  }
 
   void calibrate() {
     int initialPos = cap.get(cv::CAP_PROP_POS_FRAMES); // Satisfy requirements: doesn't affect the passed-in VideoCapture
+    
     cv::Mat frame = getNextFrame();
 
     std::unique_ptr<std::vector<cv::Point2f>> relativePrevPoints = getPositionalIndependent(*pct.getPoints());
+    pct.update(frame);
+    std::unique_ptr<std::vector<cv::Point2f>> relativeCurrPoints = getPositionalIndependent(*pct.getPoints());
 
+    // TODO: Find motion based on the movement of the averages of the points and normalize the losses based on this?
     
+    // Calulate the translation vector between the two frames
+    cv::Mat translation = cv::findTransformECC(*relativePrevPoints, *relativeCurrPoints, cv::noArray());
+    // 
 
 
 
